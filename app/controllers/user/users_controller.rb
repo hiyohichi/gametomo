806f8c4c78a20df393c9a7e13bf04822f47f85dc
hiyohichi <hiyohichi07@gmail.com>
2024-01-14 12:03:23 +0000

    bootstrap,header作成,ゲストログイン

--- app/controllers/user/users_controller.rb
+++ app/controllers/user/users_controller.rb
@@ -1,3 +1,14 @@
 class User::UsersController < ApplicationController
   before_action :authenticate_user!,except:[:index]
+  before_action :ensure_guest_user, only: [:edit]
+  
+  
+  private
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.guest_user?
+      redirect_to user_path(current_user) , notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
+    end
+  end  
 end
