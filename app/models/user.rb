90550cc3fc55f7e7b9a2b5d86647776beb6e9aaf
hiyohichi <hiyohichi07@gmail.com>
2024-01-27 06:31:56 +0000

    ゲストユーザー紹介文

--- app/models/user.rb
+++ app/models/user.rb
@@ -69,7 +69,7 @@ class User < ApplicationRecord
     followings.include?(user)
   end
 
-  
+
   #ジャンルを区切る
   def platform()
     lis = []
@@ -86,6 +86,7 @@ class User < ApplicationRecord
     find_or_create_by!(email: GUEST_USER_EMAIL) do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = "guestuser"
+      user.introduction = "ゲストユーザーです"
     end
   end
 
